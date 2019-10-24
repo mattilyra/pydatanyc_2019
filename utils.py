49228be0e3771341feda8935d7ec90844b04bd24
Matti Lyra <matti.lyra@comtravo.com>
2019-10-24 20:38:41 +0200

    Added utils

---
+++ utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def load_gnad():
+    with open('./data/10kgnad/train.csv', 'r') as fh:
+        data = (line.split(';', 1) for line in fh)
+        gnad_train = pd.DataFrame.from_records(data, columns=['category', 'text'])
+    
+    with open('./data/10kgnad/test.csv', 'r') as fh:
+        data = (line.split(';', 1) for line in fh)
+        gnad_test = pd.DataFrame.from_records(data, columns=['category', 'text'])
+        
+    return gnad_train, gnad_test
